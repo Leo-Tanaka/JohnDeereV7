08b264579dc45e86387b8024b1d3159b764d0bcd
Bruno Seidi Ferreira Takaya <rm554986@fiap.com.br>
2024-09-22 13:47:31 -0300

    Cadastro

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,29 @@ if (username == '' || username == null) {
 
 var welcome = document.getElementById('Welcome');
 welcome.innerHTML = 'Olá, ' + username + '!';
+
+document.getElementById('form-cadastro').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const payload = JSON.stringify({
+      nome: nome,
+      email: email,
+      senha: senha
+    });
+
+    // Conectar ao HiveMQTT Broker (via WebSockets)
+    const client = mqtt.connect('wss://broker.hivemq.com:8000/mqtt'); // Usando WebSocket
+
+    client.on('connect', function() {
+      console.log('Conectado ao HiveMQTT');
+
+      // Publicar os dados no tópico de cadastro
+      client.publish('cadastro/usuarios', payload, function() {
+        alert('Cadastro enviado com sucesso!');
+      });
+    });
+  });
